e571b70632509eb651e56fbb93fe1b81791dd2df
FabiolaHernandez <A01363207@itesm.mx>
2018-09-12 12:58:04 -0500

    MapKit progress
    Implementation of annotations

--- AppHomeMapSpriteKit/LocationSearchTable.swift
+++ AppHomeMapSpriteKit/LocationSearchTable.swift
@@ -13,18 +13,24 @@ class LocationSearchTable : UITableViewController {
     var matchingItems : [MKMapItem] = [];
     // handler to the map from prev screen
     var mapView : MKMapView? = nil;
+    // locationHandler
+    var handleMapSearchDelegate:HandleMapSearch? = nil;
 }
 
 // extension for the locationSearchTable
 extension LocationSearchTable : UISearchResultsUpdating {
     func updateSearchResults(for searchController: UISearchController) {
         /* Set up API call */
-        guard let mapView = mapView,
-            let searchBarText = searchController.searchBar.text else { return }
+        //  unwraps the optional values for mapView and the search bar text
+        guard let mapView = mapView, let searchBarText = searchController.searchBar.text else { return }
+        // The search string comes from the search bar text, and the map region comes from the mapView.
         let request = MKLocalSearchRequest()
         request.naturalLanguageQuery = searchBarText
         request.region = mapView.region
+        
+        // performs the actual search on the request object
         let search = MKLocalSearch(request: request)
+        // start, executes the search query and returns a MKLocalSearchResponse object
         search.start { response, _ in
             guard let response = response else {
                 return
@@ -34,3 +40,27 @@ extension LocationSearchTable : UISearchResultsUpdating {
         }
     }
 }
+
+/* Set up the Table View Data Source */
+extension LocationSearchTable {
+    override func tableView(_ tableView: UITableView, numberOfRowsInSection section: Int) -> Int {
+        return matchingItems.count
+    }
+    
+    override func tableView(_ tableView: UITableView, cellForRowAt indexPath: IndexPath) -> UITableViewCell
+    {
+        let cell = tableView.dequeueReusableCell(withIdentifier: "cell")!
+        let selectedItem = matchingItems[indexPath.row].placemark;
+        cell.textLabel?.text = selectedItem.name;
+        cell.detailTextLabel?.text = ""; // Change the detail of place or change cell to Title only
+        return cell;
+    }
+    override func tableView(_ tableView: UITableView, didSelectRowAt indexPath: IndexPath){
+        let selectedItem = matchingItems[indexPath.row].placemark
+        handleMapSearchDelegate?.dropPinZoomIn(placemark: selectedItem)
+        dismiss(animated: true, completion: nil)
+    }
+}
+
+
+
--- AppHomeMapSpriteKit/ViewController.swift
+++ AppHomeMapSpriteKit/ViewController.swift
@@ -9,52 +9,60 @@
 import UIKit
 import MapKit
 
+/* Custom protocol */
+protocol HandleMapSearch {
+    func dropPinZoomIn(placemark:MKPlacemark)
+}
+
 class ViewController : UIViewController {
-    /* CLLocationManager gives access to the location manager throughout the  scope of the controller
-     */
+    // CLLocationManager gives access to the location manager throughout the  scope of the controller
     let locationManager = CLLocationManager()
     // mapKitVariable
     @IBOutlet weak var mapView: MKMapView!
     // searchBar
     var resultSearchController : UISearchController? = nil;
+    // pin
+    var selectedPin:MKPlacemark? = nil;
     
     override func viewDidLoad() {
         super.viewDidLoad()
         
         /* Get current location of user */
         // delegate, handles responses asynchronously
-        locationManager.delegate = self; // then call the extension
-        
+        locationManager.delegate = self;
         // (optional) override the default accuracy level
-        locationManager.desiredAccuracy = kCLLocationAccuracyBest;
         
-            // or to save some battery
-            //locationManager.desiredAccuracy = kCLLocationAccuracyHundredMeters;
+        // or to save some battery
+        //locationManager.desiredAccuracy = kCLLocationAccuracyHundredMeters;
         // triggers the location permission dialog
+        locationManager.desiredAccuracy = kCLLocationAccuracyBest;
+        // triggers a one-time location request
         locationManager.requestWhenInUseAuthorization();
         
-        // triggers a one-time location request
-        locationManager.requestLocation();
-        
-        /* Search bar */
+        /* Search Bar */
         // uses storyboardID; Table View Controller, works as a delegate
-        let locationSearchTable = storyboard!.instantiateViewController(withIdentifier: "LocationSearchTable") as! LocationSearchTable;
-        resultSearchController = UISearchController(searchResultsController: locationSearchTable);
+        locationManager.requestLocation()
+        let locationSearchTable = storyboard!.instantiateViewController(withIdentifier: "LocationSearchTable") as! LocationSearchTable
+        resultSearchController = UISearchController(searchResultsController: locationSearchTable)
+        resultSearchController?.searchResultsUpdater = locationSearchTable
         
         // Configure searchBar & embeds it with the navigation bar
-        let searchBar = resultSearchController!.searchBar;
-        searchBar.sizeToFit();
-        searchBar.placeholder = "Look for places";
-        navigationItem.titleView = resultSearchController?.searchBar;
+        let searchBar = resultSearchController!.searchBar
+        searchBar.sizeToFit()
+        searchBar.placeholder = "Ingresa direccion"
+        navigationItem.titleView = resultSearchController?.searchBar
         // determines whether the Navigation Bar dissapears when search results are shown
-        resultSearchController?.hidesNavigationBarDuringPresentation = false;
+        resultSearchController?.hidesNavigationBarDuringPresentation = false
         // gives semi-transparent look to the background searchBar is selected
-        resultSearchController?.dimsBackgroundDuringPresentation = true;
+        resultSearchController?.dimsBackgroundDuringPresentation = true
         // limits the overlap area to just the View Controller's frame
-        definesPresentationContext = true;
+        definesPresentationContext = true
         
         /* Link map elements */
-        locationSearchTable.mapView = mapView;
+        locationSearchTable.mapView = mapView
+        
+        /* pin */
+        locationSearchTable.handleMapSearchDelegate = self;
     }
 }
 // in extension intead another class
@@ -85,3 +93,26 @@ extension ViewController : CLLocationManagerDelegate {
     }
 }
 
+/* extension for the pin */
+extension ViewController: HandleMapSearch {
+    // dropPinZoomIn for the Handle protocol created
+    func dropPinZoomIn(placemark:MKPlacemark){
+        // cache the pin
+        selectedPin = placemark
+        // clear existing pins
+        mapView.removeAnnotations(mapView.annotations)
+        
+        // create a new annotation
+        let annotation = MKPointAnnotation()
+        annotation.coordinate = placemark.coordinate
+        annotation.title = placemark.name
+        if let city = placemark.locality,
+            let state = placemark.administrativeArea {
+            annotation.subtitle = "Cuidad: \(city) Estado: \(state)";
+        }
+        mapView.addAnnotation(annotation)
+        let span = MKCoordinateSpanMake(0.05, 0.05)
+        let region = MKCoordinateRegionMake(placemark.coordinate, span)
+        mapView.setRegion(region, animated: true)
+    }
+}
